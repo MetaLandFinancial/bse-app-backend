25f2274d0b87ca426618bc9429614789429310cd
slowpon <slowpon@outlook.com>
2024-06-17 15:35:28 -0400

    sign in controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Post, Body, HttpCode, HttpStatus } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { UserSignInDto } from './dto/user-sign-in.dto';
 import { User } from './user.entity';
 
 @Controller('users')
@@ -9,7 +10,25 @@ export class UserController {
 
   @Post('sign-up')
   @HttpCode(HttpStatus.CREATED)
-  async signUp(@Body() createUserDto: CreateUserDto): Promise<User> {
-    return this.userService.createUser(createUserDto);
+  async signUp(@Body() createUserDto: CreateUserDto): Promise<any> {
+    const user = this.userService.createUser(createUserDto);
+    return {
+      statusCode: 201,
+      status: 'success',
+      message: 'Sign up successfully',
+      data: user,
+    };
+  }
+
+  @Post('sign-in')
+  @HttpCode(HttpStatus.CREATED)
+  async signIn(@Body() userSignInDto: UserSignInDto): Promise<any> {
+    const user = this.userService.signIn(userSignInDto);
+    return {
+      statusCode: 200,
+      status: 'success',
+      message: 'Sign in successfully',
+      data: user,
+    };
   }
 }
